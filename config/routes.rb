8bfae010391678d0fa2a4306c4f2d1d97ff23f95
vicgphillips1 <vicgphillips1@gmail.com>
2015-05-05 14:42:49 -0700

    updated products

--- config/routes.rb
+++ config/routes.rb
@@ -6,21 +6,14 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-  get 'static_pages/about'
+  resources :orders, only: [:index, :show, :new, :create]
 
-  get 'static_pages/contact'
-
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
-
-  get 'static_pages/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'static_pages#index'
+   root 'static_pages#landing_page'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
