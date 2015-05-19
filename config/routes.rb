b4f19662d9b4f112c70b83a5fa12f2d4a4860abf
vicgphillips1 <vicgphillips1@gmail.com>
2015-05-19 13:41:09 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :new, :create]
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
+
+  post 'static_pages/thank_you'
 
   resources :orders, only: [:index, :show, :new, :create]
 
@@ -14,6 +17,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'static_pages#landing_page'
+   resources :orders, only: [:index, :show, :new, :create]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
