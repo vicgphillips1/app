f558c8d16a5c20d47069f2475bf9a47796deadd5
vicgphillips1 <vicgphillips1@gmail.com>
2015-05-29 14:03:21 -0700

    add contact form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,22 @@
 class StaticPagesController < ApplicationController
-
+  def about
+  end
   def index
   end
-
-  def landing_page
-    @products = Product.limit(3)
+  def contact
   end
-
+  def landing_page
+   @featured_product = Product.first
+   @products = Product.limit(3)
+  end
+ def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email, 
+	  	:to => 'vicgphillips1@gmail.com', 
+	  	:subject => "A new contact form message from #{@name}", 
+	  	:body => @message).deliver
 end
+end
+
