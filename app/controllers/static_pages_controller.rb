a412c30d22c1096c3b8068d31f2c26b54d0c80ee
vicgphillips1 <vicgphillips1@gmail.com>
2015-05-31 15:13:00 -0700

    updated contact form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,20 +1,21 @@
-class StaticPagesController < ApplicationController
-  def about
-  end
 
-  def contact
+class StaticPagesController < ApplicationController
+  def landing_page
+    @products = Product.limit(3)
   end
 
   def thank_you
-    @name = params[:name]
-    @email = params[:email]
-    @message = params[:message]
-    UserMailer.contact_form(@name, @email, @message).deliver
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  UserMailer.contact_form(@email, @name, @message).deliver
+  ActionMailer::Base.mail(:from => @email, 
+      :to => 'vicgphillips1@gmail.com', 
+      :subject => "A new contact form message from #{@name}", 
+      :body => @message).deliver
   end
 
-  def landing_page
-    @featured_product = Product.first
-    @products = Product.limit(3)
+  def index
   end
 end
 
