0473352ea1067f223d75f301c5c97ef5a422f658
Vic Phillips M.D. <vicgphillips1@gmail.com>
2015-06-17 11:07:03 -0700

    Update static_pages_controller.rb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,13 +10,6 @@ def thank_you
   @name = params[:name]
   @email = params[:email]
   @message = params[:message]
-  ActionMailer::Base.mail(:from => @email, :to => 'vicgphillips1@gmail.com', :subject => ' #@name sent you a message, man!', :body => @message).deliver
-end
-
-def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
   UserMailer.contact_form(@email, @name, @message).deliver
 end
 
